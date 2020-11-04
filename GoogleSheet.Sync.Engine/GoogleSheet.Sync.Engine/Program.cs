83a0d3ddebe6562f7ddc45cb6cf0186f23b0de90
Ganeshram Ravi <ganeshram.ravi@rrd.com>
2020-11-04 12:44:53 +0530

    Google Sheet Synchronization Engine.

---
+++ GoogleSheet.Sync.Engine/GoogleSheet.Sync.Engine/Program.cs
@@ -0,0 +1,104 @@
+﻿
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+
+namespace GoogleSheet.Sync.Engine
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "Google Sheets API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            //Write Operation
+
+            ///1bQ-B-lCefPIMFHQeobUygF5xP52gI1ved3sbiOwjHrE
+            string sheetId = "1bQ-B-lCefPIMFHQeobUygF5xP52gI1ved3sbiOwjHrE";
+            string range = "Sheet1!A2:B4";
+
+            var serviceValues = service.Spreadsheets.Values;
+            SpreadsheetsResource.ValuesResource valuesResource = serviceValues;
+
+            List<IList<object>> data = new List<IList<object>>();
+            var valueRange = new ValueRange
+            {
+                Values = new List<IList<object>> 
+                {
+                    new List<object> { "ram1", "PR1" },
+                    new List<object> { "ram2", "PR2" },
+                    new List<object> { "ram3", "PR3" }
+                }
+            };
+            var update = valuesResource.Update(valueRange, sheetId, range);
+            update.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+            var response = update.ExecuteAsync();
+            Console.WriteLine("Write Completed");
+
+            //Reading the values 
+            // Define request parameters.
+            //11U_7wlwRBpESWV-n8AQ77i7PmUSJy0xCT5-hSdexapI
+            //String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+            //String range = "Class Data!A2:E";
+            //SpreadsheetsResource.ValuesResource.GetRequest request =
+            //        service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            //// Prints the names and majors of students in a sample spreadsheet:
+            //// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            //ValueRange response = request.Execute();
+            //IList<IList<Object>> values = response.Values;
+            //if (values != null && values.Count > 0)
+            //{
+            //    Console.WriteLine("Name, Major");
+            //    foreach (var row in values)
+            //    {
+            //        // Print columns A and E, which correspond to indices 0 and 4.
+            //        Console.WriteLine("{0}, {1}", row[0], row[4]);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No data found.");
+            //}
+            Console.Read();
+        }
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+    }
+}
---
+++ GoogleSheet.Sync.Engine/GoogleSheet.Sync.Engine/SpreedSheetConnector.cs
@@ -0,0 +1,65 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace GoogleSheet.Sync.Engine
+{
+
+    public class SpreadSheetConnector
+    {
+
+        private string[] _scopes = { SheetsService.Scope.Spreadsheets }; // Change this if you're accessing Drive or Docs
+        private string _applicationName = "My Application Name from Google API Project ";
+        private string _spreadsheetId = "xdMsqBc3wblahblahblahblahkeygoeshere";
+        private SheetsService _sheetsService;
+
+        private void ConnectToGoogle()
+        {
+            GoogleCredential credential;
+
+            // Put your credentials json file in the root of the solution and make sure copy to output dir property is set to always copy 
+            using (var stream = new FileStream("credentials.json",
+                FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(_scopes);
+            }
+
+            // Create Google Sheets API service.
+            _sheetsService = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = _applicationName
+            });
+        }
+
+        // Pass in your data as a list of a list (2-D lists are equivalent to the 2-D spreadsheet structure)
+        public string UpdateData(List<IList<object>> data)
+        {
+            String range = "My Tab Name!A1:Y";
+            string valueInputOption = "USER_ENTERED";
+
+            // The new values to apply to the spreadsheet.
+            List<Google.Apis.Sheets.v4.Data.ValueRange> updateData = new List<Google.Apis.Sheets.v4.Data.ValueRange>();
+            var dataValueRange = new Google.Apis.Sheets.v4.Data.ValueRange();
+            dataValueRange.Range = range;
+            dataValueRange.Values = data;
+            updateData.Add(dataValueRange);
+
+            Google.Apis.Sheets.v4.Data.BatchUpdateValuesRequest requestBody = new Google.Apis.Sheets.v4.Data.BatchUpdateValuesRequest();
+            requestBody.ValueInputOption = valueInputOption;
+            requestBody.Data = updateData;
+
+            var request = _sheetsService.Spreadsheets.Values.BatchUpdate(requestBody, _spreadsheetId);
+
+            Google.Apis.Sheets.v4.Data.BatchUpdateValuesResponse response = request.Execute();
+            // Data.BatchUpdateValuesResponse response = await request.ExecuteAsync(); // For async 
+
+            return JsonConvert.SerializeObject(response);
+        }
+    }
+}
